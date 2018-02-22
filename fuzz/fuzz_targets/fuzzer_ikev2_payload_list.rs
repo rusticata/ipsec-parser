8d4608cf7f6c50a757c899d3185c2d5971bc9703
Pierre Chifflier <chifflier@wzdftpd.net>
2018-02-22 08:52:58 +0100

    Update fuzzers for newtypes

--- fuzz/fuzz_targets/fuzzer_ikev2_payload_list.rs
+++ fuzz/fuzz_targets/fuzzer_ikev2_payload_list.rs
@@ -5,6 +5,6 @@ extern crate ipsec_parser;
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
     if data.len() > 0 {
-        let _ = ipsec_parser::parse_ikev2_payload_list(&data[1..],data[0]);
+        let _ = ipsec_parser::parse_ikev2_payload_list(&data[1..],ipsec_parser::IkePayloadType(data[0]));
     }
 });
